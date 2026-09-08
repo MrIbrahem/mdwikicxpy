a479f5a394d4ae76ffb589481125be91e99348a0
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-08 06:53:02 +0300

    .

--- tests/unit/lib/lineardoc/test_doc_as_js.py
+++ tests/unit/lib/lineardoc/test_doc_as_js.py
@@ -1,13 +1,12 @@
 import json
-import re
 from pathlib import Path
 
 from python.lib.lineardoc import (
     MwContextualizer,
-    Normalizer,
     Parser,
     TextBlock,
 )
+from tests.unit.html_normalizer import normalize_test
 
 FIXTURES_DIR = Path(__file__).resolve().parent / "data"
 
@@ -20,16 +19,6 @@ with open(trans_tests_path, "r", encoding="utf-8") as f:
 def deep_equal(result, expected, desc):
     assert result == expected, desc
 
-
-def normalize(html: str) -> str:
-    normalizer = Normalizer()
-    normalizer.init()
-    # Replace \r\n, \n, \t, or \r globally
-    cleaned_html = re.sub(r"(\r\n|\n|\t|\r)", "", html)
-    normalizer.write(cleaned_html)
-    return normalizer.get_html()
-
-
 class TestLinearDoc:
 
     def test_should_be_possible_to_linearise_all_kind_of_html_inputs(self):
@@ -40,13 +29,13 @@ class TestLinearDoc:
             result_xhtml_file = FIXTURES_DIR / f"/test{i}-result.xhtml"
 
             with open(test_xhtml_file, "r", encoding="utf-8") as f:
-                test_xhtml = re.sub(r"^\s+|\s+$", "", f.read())
+                test_xhtml = f.read()
 
             with open(result_xml_file, "r", encoding="utf-8") as f:
-                expected_xml = re.sub(r"^\s+|\s+$", "", f.read())
+                expected_xml = f.read()
 
             with open(result_xhtml_file, "r", encoding="utf-8") as f:
-                expected_xhtml = re.sub(r"^\s+|\s+$", "", f.read())
+                expected_xhtml = f.read()
 
             parser = Parser(MwContextualizer())
             parser.init()
@@ -54,8 +43,8 @@ class TestLinearDoc:
 
             assert not parser.builder.doc.is_ignorable_block(), "Not a section with block template"
 
-            deep_equal(normalize(parser.builder.doc.dump_xml()), normalize(expected_xml), "Linearised structure")
-            deep_equal(normalize(parser.builder.doc.get_html()), normalize(expected_xhtml), "Reconstructed XHTML")
+            deep_equal(normalize_test(parser.builder.doc.dump_xml()), normalize_test(expected_xml), "Linearised structure")
+            deep_equal(normalize_test(parser.builder.doc.get_html()), normalize_test(expected_xhtml), "Reconstructed XHTML")
 
     def test_should_be_possible_to_reconstruct_the_html_from_lineardoc(self):
         for test in trans_tests:
@@ -71,77 +60,6 @@ class TestLinearDoc:
             # text_block2 = text_block1.translate_tags(test["targetText"], test["rangeMappings"])
             # deep_equal(text_block2.get_html(), test["expect"], "Translated HTML")
 
-    def test_should_be_possible_to_reduce_and_expand_a_document(self):
-        test_xhtml_file = FIXTURES_DIR / "test-figure-inline.html"
-        with open(test_xhtml_file, "r", encoding="utf-8") as f:
-            content_for_reduce = re.sub(r"^\s+|\s+$", "", f.read())
-
-        parser = Parser(MwContextualizer())
-        parser.init()
-        parser.write(content_for_reduce)
-
-        data = parser.builder.doc.reduce()
-        reduced_doc, extracted_data = data["reduced_doc"], data["extracted_data"]
-
-        deep_equal(len(extracted_data.keys()), 16, "Attributes for 16 tags extracted.")
-
-        expanded_doc = reduced_doc.expand(extracted_data)
-        deep_equal(
-            normalize(expanded_doc.get_html()),
-            normalize(content_for_reduce),
-            "Restored the original html after reduce and expand.",
-        )
-
-    def test_html_compaction_roundtrip_with_inline_chunks(self):
-        test_xhtml_file = FIXTURES_DIR / "test-chunks-inline.html"
-        with open(test_xhtml_file, "r", encoding="utf-8") as f:
-            content_for_reduce = re.sub(r"^\s+|\s+$", "", f.read())
-
-        parser = Parser(MwContextualizer())
-        parser.init()
-        parser.write(content_for_reduce)
-
-        data = parser.builder.doc.reduce()
-        reduced_doc, extracted_data = data["reduced_doc"], data["extracted_data"]
-
-        deep_equal(len(extracted_data.keys()), 22, "Attributes for 22 tags extracted.")
-
-        expanded_doc = reduced_doc.expand(extracted_data)
-        deep_equal(
-            normalize(expanded_doc.get_html()),
-            normalize(content_for_reduce),
-            "Restored the original html after reduce and expand.",
-        )
-
-    def test_html_expand_with_external_attributes_inserted(self):
-        corrupted_doc = """<p id="1">
-            <b id="2" onclick="doSomething();">Externally inserted attribute.</b>
-            <a href="navigateThere();">Externally inserted tag</a>
-            <span id="mwEz">Element with only id attribute is fine.</span>
-            </p>"""
-
-        sanitized_expanded_doc = """<p class="paragraph" id="mwEq">
-            <b class="bold" id="mwEs">Externally inserted attribute.</b>
-            <a>Externally inserted tag</a>
-            <span id="mwEz">Element with only id attribute is fine.</span>
-            </p>"""
-
-        extracted_data = {
-            "1": {"attributes": {"id": "mwEq", "class": "paragraph"}},
-            "2": {"attributes": {"id": "mwEs", "class": "bold"}},
-        }
-
-        parser = Parser(MwContextualizer())
-        parser.init()
-        parser.write(corrupted_doc)
-
-        expanded_doc = parser.builder.doc.expand(extracted_data)
-        deep_equal(
-            normalize(expanded_doc.get_html()),
-            normalize(sanitized_expanded_doc),
-            "Expanded the corrupted document by removing all externally inserted attributes.",
-        )
-
     def test_if_the_content_is_block_level_template(self):
         test_files = [
             "test-block-template-section-1.html",
@@ -153,7 +71,7 @@ class TestLinearDoc:
         for file_path_rel in test_files:
             filepath = FIXTURES_DIR / file_path_rel
             with open(filepath, "r", encoding="utf-8") as f:
-                content_for_test = re.sub(r"^\s+|\s+$", "", f.read())
+                content_for_test = f.read()
 
             parser = Parser(MwContextualizer())
             parser.init()
@@ -164,7 +82,7 @@ class TestLinearDoc:
     def test_should_not_ignore_a_section_that_begins_with_an_inline_template_but_has_translatable_prose(self):
         filepath = FIXTURES_DIR / "test-inline-template-section.html"
         with open(filepath, "r", encoding="utf-8") as f:
-            content_for_test = re.sub(r"^\s+|\s+$", "", f.read())
+            content_for_test = f.read()
 
         parser = Parser(MwContextualizer())
         parser.init()
@@ -173,98 +91,6 @@ class TestLinearDoc:
         assert (
             not parser.builder.doc.is_ignorable_block()
         ), "Section starting with an inline transclusion followed by prose is translatable"
-
-    def test_html_compaction_roundtrip_with_inline_style_content(self):
-        source_doc = """<section><p>
-        <a href="Our title">ABC</a>
-        <style> a { background: url(https://en.wikipedia.org/css-background); } </style>
-        <script>original script</script>
-        <script type="module" src="main.js"></script>
-        <span id="mwKJ" typeof="mw:Entity">&ndash;</span>
-        </p>
-        </section>"""
-
-        expected_reduced_doc = """<section><p>
-        <a id="1">ABC</a>
-        <style id="2"></style>
-        <script id="3"></script>
-        <script id="4"></script>
-        <span id="5">&ndash;</span>
-        </p>
-        </section>"""
-
-        # MT result from external MT service. Assuming that it altered the style and script content.
-        corrupted_mt_input = """<section><p>
-        <a id="1" href="Their title">abc</a>
-        <style id="2"> a { background: url(https://leaking.via/css-background); } </style>
-        <script id="3">Corrupted script</script>
-        <script id="4" src="https://bad.via/main.js"></script>
-        <span id="5">©</span>
-        </p>
-        </section>"""
-
-        # Expected final output after fixing all external modification
-        sanitized_expanded_doc = """<section><p>
-        <a href="Our title">abc</a>
-        <style> a { background: url(https://en.wikipedia.org/css-background); } </style>
-        <script>original script</script>
-        <script type="module" src="main.js"></script>
-        <span id="mwKJ" typeof="mw:Entity">&ndash;</span>
-        </p>
-        </section>"""
-
-        parser = Parser(MwContextualizer())
-        parser.init()
-        parser.write(source_doc)
-
-        data = parser.builder.doc.reduce()
-        reduced_doc, extracted_data = data["reduced_doc"], data["extracted_data"]
-
-        deep_equal(
-            normalize(reduced_doc.get_html()),
-            normalize(expected_reduced_doc),
-            "Expanded the corrupted document by removing all externally inserted attributes.",
-        )
-        deep_equal(len(extracted_data.keys()), 5, "Attributes for 2 tags extracted.")
-        deep_equal(not not extracted_data["2"].get("content"), True, "Content extracted for style tag")
-        deep_equal(not not extracted_data["3"].get("content"), True, "Content extracted for script tag")
-        deep_equal(not not extracted_data["5"].get("content"), True, "Content extracted for tag with mw:Entity")
-
-        parser = Parser(MwContextualizer())
-        parser.init()
-        parser.write(corrupted_mt_input)
-
-        expanded_doc = parser.builder.doc.expand(extracted_data)
-        deep_equal(
-            normalize(expanded_doc.get_html()),
-            normalize(sanitized_expanded_doc),
-            "Expanded the corrupted document by ignoring modified style and script content",
-        )
-
-    def test_html_compaction_roundtrip_with_template_with_empty_content(self):
-        test_xhtml_file = FIXTURES_DIR / "text-inline-template-empty-content.html"
-        with open(test_xhtml_file, "r", encoding="utf-8") as f:
-            content_for_reduce = re.sub(r"^\s+|\s+$", "", f.read())
-
-        parser = Parser(MwContextualizer())
-        parser.init()
-        parser.write(content_for_reduce)
-
-        data = parser.builder.doc.reduce()
-        reduced_doc, extracted_data = data["reduced_doc"], data["extracted_data"]
-
-        reduced_parser = Parser(MwContextualizer())
-        reduced_parser.init()
-        reduced_parser.write(reduced_doc.get_html())
-
-        deep_equal(len(extracted_data.keys()), 6, "Attributes for 6 tags extracted.")
-
-        expanded_doc = reduced_parser.builder.doc.expand(extracted_data)
-        deep_equal(
-            normalize(expanded_doc.get_html()),
-            normalize(content_for_reduce),
-            "Restored the original html after reduce and expand.",
-        )
 
     def test_get_root_item_for_ignoring_blockspaces(self):
         source_doc = """<section data-mw-section-id="25" id="mwArE">
